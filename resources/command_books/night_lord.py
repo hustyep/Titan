47296783faeee7592a6ef62463fb41d3b15c5ce0
win11\husty <hustyep@gmail.com>
2023-12-02 01:43:00 +0800

    1

--- resources/command_books/night_lord.py
+++ resources/command_books/night_lord.py
@@ -94,7 +94,7 @@ def step(target, tolerance):
     elif abs(d_x) >= 26:
         hit_and_run(direction, next_p, tolerance)
     elif abs(d_x) >= 10 and ShadowSurge.ready:
-        ShadowSurge().execute()
+        ShadowSurge(direction).execute()
     else:
         Walk(target_x=next_p[0], tolerance=tolerance).execute()
 
@@ -251,7 +251,7 @@ class DarkFlare(Skill):
     key = Keybindings.DARK_FLARE
     type = SkillType.Summon
     cooldown = 58
-    backswing = 1
+    backswing = 0.8
     duration = 60
 
     def __init__(self, direction=None):
